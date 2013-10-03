19b844f6434237d1baa4ecb1ffa8ad6fb4f195c3
pellele13 <lrpellegrino@gmail.com>
2013-10-03 17:00:34 -0300

    Add Test package and class price Test.

---
+++ src/com/Test/PrecioTest.java
@@ -0,0 +1,19 @@
+package com.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrecioTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
